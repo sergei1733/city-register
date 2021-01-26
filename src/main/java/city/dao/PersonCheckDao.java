dffe978ba4c8d021a42effebe627cbfc136dab71
sergei1733 <sergei1733@yandex.ru>
2021-01-26 22:47:16 +0500

    city

--- src/main/java/city/dao/PersonCheckDao.java
+++ src/main/java/city/dao/PersonCheckDao.java
@@ -11,12 +11,27 @@ import java.sql.SQLException;
 
 public class PersonCheckDao {
 
-    private final String SQL_REQUEST = "";
+    private final String SQL_REQUEST = "SELECT temporal" +
+            "from cr_address_person ap" +
+            "inner join cr_person p on p.person_id = ap.person_id" +
+            "inner join cr_address a on a.address_id = ap.address_id" +
+            "where" +
+            "upper(p.sur_name) = upper(?)  and" +
+            "upper(p.given_name) = upper(?) and" +
+            "upper(patronymic) = upper(?)and" +
+            "p.date_of_birth = ?" +
+            "and a.street_code = ? and" +
+            "upper(a.building) = upper(?) and" +
+            "upper(extension) =upper(?) and" +
+            "upper(a.apartment) =upper(?)";
 
     public PersonResponse checkPerson (PersonRequest request) throws PersonCheckException {
         PersonResponse response = new PersonResponse();
         try (Connection con = getConnection();
              PreparedStatement stmt = con.prepareStatement(SQL_REQUEST)){
+
+            stmt.setString(1,request.getSurName());
+
 
             ResultSet rs = stmt.executeQuery();
             if(rs.next()){
