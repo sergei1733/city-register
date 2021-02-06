6cd3b5257b4c36fe422af7bdcab560ee2342f5a2
sergei1733 <sergei1733@yandex.ru>
2021-02-06 23:37:16 +0500

    pom

--- src/main/java/city/web/CheckPersonServlet.java
+++ src/main/java/city/web/CheckPersonServlet.java
@@ -12,6 +12,6 @@ public class CheckPersonServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("Get CheckPerson - called");
+
     }
 }
