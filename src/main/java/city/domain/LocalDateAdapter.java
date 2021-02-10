4de2afa25231c90149c282e6901ef700bb80d8de
sergei1733 <sergei1733@yandex.ru>
2021-02-10 22:13:58 +0500

    rest

---
+++ src/main/java/city/domain/LocalDateAdapter.java
@@ -0,0 +1,17 @@
+package city.domain;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateAdapter extends XmlAdapter<String, LocalDate> {
+    @Override
+    public LocalDate unmarshal(String v) throws Exception {
+        return LocalDate.parse(v, DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    @Override
+    public String marshal(LocalDate v) throws Exception {
+        return v.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+}
--- src/main/java/city/web/CheckPersonService.java
+++ src/main/java/city/web/CheckPersonService.java
@@ -1,18 +1,21 @@
 package city.web;
 
+import city.domain.PersonRequest;
 import city.domain.PersonResponse;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.awt.*;
+import java.time.LocalDate;
 
 @Path("/check")
-@Produces(MediaType.APPLICATION_JSON)
 public class CheckPersonService {
-    @GET
-    @Path("/{id}")
-    public PersonResponse checkPerson(@PathParam ("id")int simpleId,
-                                      @QueryParam("name") String simpleName){
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public PersonResponse checkPerson(PersonRequest request){
+        System.out.println(request.toString());
         return new PersonResponse();
     }
 }
